1b8163a9a01b1a20f9b60e60e6da9127a078a515
zacanger <zac@zacanger.com>
2019-05-03 10:20:32 -0600

    Fixes

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,10 +1,11 @@
 /* eslint-env jest */
 
-import { net } from './net'
+import { net } from './'
 
 test('net should have all default headers on initialization', (): void => {
   // @ts-ignore
   window.fetch = (url, opts) => Promise.resolve({ url, ...opts })
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   net('/').then(({ headers }): void => {})
 })
