4e22b86a04b2c30c64741c01b3ffcc8e6941dbca
zacanger <zac@zacanger.com>
2019-07-06 15:21:01 -0600

    Bundle types

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import ts from 'rollup-plugin-typescript'
+import ts from 'rollup-plugin-typescript2'
 import json from 'rollup-plugin-json'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
